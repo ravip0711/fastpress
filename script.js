5038116e6e8b312251346eddbab9c17636b9a7ff
Ravi P <ravip0711@gmail.com>
2016-05-30 18:59:36 -0700

    added second player keypress functionality

--- script.js
+++ script.js
@@ -1,16 +1,21 @@
-// player1 keypress
-// $(document).on('keydown', function() {
 
-//   $('.player1:last').after('<div class="player1">Go!<div>');
-// })
 
 function handleKeyDown(event) {
-  if ( event.which == 81) {
-    $('.player1:last').after('<div class="player1">Go!<div>');
-  };
+  var eventKey = event.which;
+  switch (eventKey) {
+    // "q" = 81 in keyCode
+    case 81:
+      $('.player1:last').after('<div class="player1">Go!<div>');
+      break;
+    // "p" = 80 in keyCode 
+    case 80:
+      $('.player2:last').after('<div class="player2">Go!<div>');
+      break;  
+  }
 }
 
-var $keyPressedAction = $(document).one('keydown', handleKeyDown)
+
+var $addStep = $(document).one('keydown', handleKeyDown)
 .on('keyup', function() {
     $(document).one('keydown', handleKeyDown(event));
 })
