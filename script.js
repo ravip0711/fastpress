9d16f312b639af871f9911a152a73ac1d01a5648
Ravi P <ravip0711@gmail.com>
2016-06-02 00:46:19 -0700

    added logic for player who get to 10 boxes first wins and resetting the board

--- script.js
+++ script.js
@@ -1,21 +1,55 @@
+// function getMarkers() {
+//   var player1 = prompt("Player 1, enter your key.")
+//   var player2 = prompt("player 2, enter your key.")
+// }
 
+// function updateCounter(player){
+//   var playerOneCounter = 0;
+//   var playerTwoCounter = 0;
+
+//   function() {
+//     if (player == "player1") {
+//       playerOneCounter += 1;
+//     } else if (player == "player2") {
+//       playerTwoCounter += 1;  
+//     }
+//   }
+// }
 
 function handleKeyDown(event) {
   var eventKey = event.which;
   switch (eventKey) {
-    // "q" = 81 in keyCode
-    case 81:
+    case 81: // "q" = 81 in keyCode
       $('.player1:last').after('<div class="player1">Go!<div>');
+      // updateCounter("player1");
+      winner();
       break;
-    // "p" = 80 in keyCode 
-    case 80:
+    case 80: // "p" = 80 in keyCode 
       $('.player2:last').after('<div class="player2">Go!<div>');
-      break;  
+      // updateCounter("player2");
+      winner();
+      break;
   }
 }
 
-
-var $addStep = $(document).one('keydown', handleKeyDown)
+var $addStep = $(document).one('keydown')
 .on('keyup', function() {
     $(document).one('keydown', handleKeyDown(event));
 })
+
+function resetBoard() {
+  alert("reset board?")
+  $('.player1').slice(1, 11).remove();
+  $('.player2').slice(1, 11).remove();
+}
+
+function winner() {
+  if ($('.player1').length >= 11) {
+    alert("Player 1 is the Winner!");
+    resetBoard();
+  }
+  if ($('.player2').length >= 11) {
+    alert("Player 2 is the Winner!");
+    resetBoard();
+  }
+}
